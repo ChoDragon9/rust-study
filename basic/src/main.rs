7ebc0497efaa749e6fb4391cfb6e97111a08cc91
Yongku Cho <04whdydrn30@gmail.com>
2024-05-24 18:45:18 +0900

    refactor: 주석 제거 및 세미콜론 추가

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -2,32 +2,32 @@
 // 함수 선언은 fn을 사용함
 fn main() {
   // todo!: 완성되지 않은 코드를 식별하는 데 사용 
-  // todo!("Display the message by using the println!() macro")
+  todo!("Display the message by using the println!() macro");
 
-  // println!("{}, {}", "A", "B")
+  println!("{}, {}", "A", "B");
 
-  // let shadow_num = 5;
-  // let shadow_num = shadow_num + 5;
-  // let shadow_num = shadow_num * 2;
-  // println!("The number is {}.", shadow_num);
+  let shadow_num = 5;
+  let shadow_num = shadow_num + 5;
+  let shadow_num = shadow_num * 2;
+  println!("The number is {}.", shadow_num);
 
-  // let number: u32 = 14;
-  // println!("The number is {}.", number);
+  let number: u32 = 14;
+  println!("The number is {}.", number);
 
-  // let number_64 = 4.0;
-  // let number_32: f32 = 5.0;
+  let number_64 = 4.0;
+  let number_32: f32 = 5.0;
 
-  // println!("1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}", 1u32 + 2, 8u32 - 5, 15 * 3);
-  // println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+  println!("1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}", 1u32 + 2, 8u32 - 5, 15 * 3);
+  println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
 
-  // let is_bigger = 1 > 4;
-  // println!("Is 1 > 4? {}", is_bigger);
+  let is_bigger = 1 > 4;
+  println!("Is 1 > 4? {}", is_bigger);
 
-  // let char_1: char = 'S';
-  // let char_2: char = 'f';
-  // let smiley_face = '🤓';
-  // let str_1 = "miley ";
-  // let str_2: &str = "ace";
+  let char_1: char = 'S';
+  let char_2: char = 'f';
+  let smiley_face = '🤓';
+  let str_1 = "miley ";
+  let str_2: &str = "ace";
 
-  // println!("{} is a {}{}{}{}.", smiley_face, char_1, str_1, char_2, str_2);
+  println!("{} is a {}{}{}{}.", smiley_face, char_1, str_1, char_2, str_2);
 }
