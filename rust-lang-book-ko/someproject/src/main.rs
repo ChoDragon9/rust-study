d65f361cad5ac2067d44e79f0ee86ce956a8caee
Yongku Cho <04whdydrn30@gmail.com>
2024-06-28 21:17:43 +0900

    feat: 연습 추가

--- rust-lang-book-ko/someproject/src/main.rs
+++ rust-lang-book-ko/someproject/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    for i500 in 0..11 {
+        for i100 in 0..4 {
+            for i50 in 0..11 {
+                let total = i500 * 500 + i100 * 100 + i50 * 50;
+                if total == 3950 {
+                    println!("500원x{}+100원x{}+50원x{}=3950", i500, i100, i50);
+                }
+            }
+        }
+    }
 }
