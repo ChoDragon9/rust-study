e70cab87fc52a7e4631116e383c3781f6e4ae3cf
Yongku Cho <04whdydrn30@gmail.com>
2024-08-25 14:40:14 +0900

    feat: 빙고 게임판

---
+++ rust-lang-book-ko/bingocard/src/main.rs
@@ -0,0 +1,27 @@
+use rand::prelude::SliceRandom;
+
+fn main() {
+    // 1에서 75까지의 숫자가 들어간 리스트를 만든다.
+    const LEN: usize = 75;
+    let mut list = [0; LEN];
+    for i in 0..LEN {
+        list[i] = i + 1;
+    }
+
+    // println!("{:?}", list);
+
+    // 리스트의 항목을 섞는다.
+    let mut rng = rand::thread_rng();
+    list.shuffle(&mut rng);
+
+    // println!("{:?}", list);
+
+    // 24개의 요소를 꺼내 빙고 카드에 할당한 뒤 출력한다.
+    let mut bingocard = [0; 24];
+
+    for i in 0..24 {
+        bingocard[i] = list[i];
+    }
+
+    println!("{:?}", bingocard);
+}
